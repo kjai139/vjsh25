bc28a43b78abd13458a64a8b24bd9757edffcec6
kj139 <therealkjai139@gmail.com>
2024-05-31 00:23:09 -0400

    start dashboard

--- src/app/_components/grid/instructionGrid.tsx
+++ src/app/_components/grid/instructionGrid.tsx
@@ -1,7 +1,8 @@
 
 import { PiCoins } from "react-icons/pi"
 import { FaPeoplePulling } from "react-icons/fa6"
-
+import { GiPodiumWinner } from "react-icons/gi"
+import { Link } from "@nextui-org/react"
 
 
 
@@ -10,22 +11,29 @@ export default function InstructionGrid() {
     const iconSize = 40
     return (
         <div className="grid grid-cols-1 sm:grid-cols-3 w-full wrapmw mt-10">
-            <div className="flex flex-col items-center justify-center igrid text-2xl">
+            
+            <Link href="/dashboard" isBlock color="foreground" className="flex flex-col items-center justify-center igrid text-2xl">
                 <PiCoins className="flex-1" size={iconSize}></PiCoins>
                 <span className="flex-1">
                     1. Acquire some coins
                 </span>
-            </div>
+            </Link>
+        
             <div className="igrid flex flex-col items-center justify-center text-2xl">
                 
                 <FaPeoplePulling className="flex-1" size={iconSize}></FaPeoplePulling>
                 
                 <span className="flex-1">
-                2. Use the coins to hire some people
+                2. Use the coins to get help
                 </span>
             </div>
-            <div className="igrid">
-                Third Block
+            <div className="igrid flex flex-col items-center justify-center text-2xl">
+                
+                <GiPodiumWinner className="flex-1" size={iconSize}></GiPodiumWinner>
+                
+                <span className="flex-1">
+                3. Achieve fluency
+                </span>
             </div>
         </div>
     )
